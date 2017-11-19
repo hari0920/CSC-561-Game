08d2fdc67d047188eaa3582d3260257ff7708356
Hariharan Ramshankar <hariharan.ramshankar@gmail.com>
2017-11-19 17:14:37 -0500

    added files

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 // Set the scene size.
-const WIDTH = 400;
-const HEIGHT = 300;
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
 
 // Set some camera attributes.
 const VIEW_ANGLE = 45;
@@ -34,3 +34,58 @@ renderer.setSize(WIDTH, HEIGHT);
 // Attach the renderer-supplied
 // DOM element.
 container.appendChild(renderer.domElement);
+
+// create a point light
+const pointLight =
+  new THREE.PointLight(0xFFFFFF);
+
+// set its position
+pointLight.position.x = 120;
+pointLight.position.y = 50;
+pointLight.position.z = 130;
+
+// add to the scene
+scene.add(pointLight);
+
+// create the sphere's material
+const sphereMaterial =
+  new THREE.MeshLambertMaterial(
+    {
+      color: 0xCC0000
+    });
+
+// Set up the sphere vars
+const RADIUS = 50;
+const SEGMENTS = 64;
+const RINGS = 64;
+
+// Create a new mesh with
+// sphere geometry - we will cover
+// the sphereMaterial next!
+const sphere = new THREE.Mesh(
+
+  new THREE.SphereGeometry(
+    RADIUS,
+    SEGMENTS,
+    RINGS),
+
+  sphereMaterial);
+
+// Move the Sphere back in Z so we
+// can see it.
+sphere.position.z = -300;
+
+// Finally, add the sphere to the scene.
+scene.add(sphere);
+
+function update ()
+{
+  // Draw!
+  renderer.render(scene, camera);
+
+  // Schedule the next frame.
+  requestAnimationFrame(update);
+}
+
+// Schedule the first frame.
+requestAnimationFrame(update);
